41f60de659a831b58184db48d28136d8750ae2cc
vvcnyy <yangwoochan68@gmail.com>
2025-08-27 16:11:39 +0900

    fix: robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,3 +3,11 @@ import type { APIRoute } from "astro";
 const robotsTxt = `
 User-agent: *
 Disallow: /`
+
+export const GET: APIRoute = () => {
+	return new Response(robotsTxt, {
+		headers: {
+			"Content-Type": "text/plain; charset=utf-8",
+		},
+	});
+};
